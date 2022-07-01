00ec73f08325bbc0bddece082c641dcd4b2346ee
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-01 19:39:25 +0200

    add task executor

--- src/kraken/cli/__main__.py
+++ src/kraken/cli/__main__.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
-from pathlib import Path
-from . import __version__
+
 import argparse
+import logging
 import sys
-from typing import Optional
+from pathlib import Path
+
 from kraken.core.build_context import BuildContext
-from slap.core.cli import Command, CliApp
+from kraken.core.build_graph import BuildGraph
+from slap.core.cli import CliApp, Command
+
+from . import __version__
 
 
 class RunCommand(Command):
@@ -14,6 +18,7 @@ class RunCommand(Command):
     class Args:
         file: Path | None
         build_dir: Path
+        verbose: bool
         targets: list[str]
 
     def init_parser(self, parser: argparse.ArgumentParser) -> None:
@@ -26,18 +31,26 @@ class RunCommand(Command):
             default=Path(".build"),
             help="the build directory to write to [default: %(default)s]",
         )
+        parser.add_argument("-v", "--verbose", action="store_true", help="always show task output and logs")
         parser.add_argument("targets", metavar="target", nargs="*", help="one or more target to build")
 
     def execute(self, args: Args) -> None:
+        from .executor import Executor
+
+        if args.verbose:
+            logging.basicConfig(level=logging.INFO, format="%(levelname)s | %(name)s | %(message)s")
+
         context = BuildContext(args.build_dir)
         context.load_project(args.file)
         targets = context.resolve_tasks(args.targets or None)
-        print(targets)
+        graph = BuildGraph(targets)
+        Executor(graph, args.verbose).execute()
 
 
 def _entrypoint() -> None:
     from kraken import core
-    app = CliApp("kraken", f"cli: {__version__}, core: {core.__version__}")
+
+    app = CliApp("kraken", f"cli: {__version__}, core: {core.__version__}", features=[])
     app.add_command("run", RunCommand())
     sys.exit(app.run())
 
