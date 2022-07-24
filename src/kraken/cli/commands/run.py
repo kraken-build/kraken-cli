74302fa35b6c62c8cbb0c56bc6e16e9bf9023a12
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-07-24 17:28:44 +0200

    feature: add `-0,--allow-no-tasks` option that allows no tasks to be selected to `kraken run` command

--- src/kraken/cli/commands/run.py
+++ src/kraken/cli/commands/run.py
@@ -9,12 +9,13 @@ from kraken.core import BuildError, Context, Task, TaskGraph
 from kraken.core.executor import COLORS_BY_RESULT
 from termcolor import colored
 
-from .base import BuildGraphCommand, print
+from .base import BuildGraphCommand
 
 
 class RunCommand(BuildGraphCommand):
     class Args(BuildGraphCommand.Args):
         skip_build: bool
+        allow_no_tasks: bool
 
     def __init__(self, main_target: str | None = None) -> None:
         super().__init__()
@@ -29,6 +30,7 @@ class RunCommand(BuildGraphCommand):
     def init_parser(self, parser: argparse.ArgumentParser) -> None:
         super().init_parser(parser)
         parser.add_argument("-s", "--skip-build", action="store_true", help="just load the project, do not build")
+        parser.add_argument("-0", "--allow-no-tasks", action="store_true", help="don't error if no tasks got selected")
 
     def resolve_tasks(self, args: BuildGraphCommand.Args, context: Context) -> list[Task]:
         if self._main_target:
@@ -45,8 +47,13 @@ class RunCommand(BuildGraphCommand):
         else:
             graph.trim()
             if not graph:
-                print(colored("Error: no tasks were selected", "red"), file=sys.stderr)
-                return 1
+                if args.allow_no_tasks:
+                    print(colored("Note: no tasks were selected (--allow-no-tasks)", "blue"), file=sys.stderr)
+                    return 0
+                else:
+                    print(colored("Error: no tasks were selected", "red"), file=sys.stderr)
+                    return 1
+
             try:
                 context.execute(graph, args.verbose > 0)
             except BuildError as exc:
